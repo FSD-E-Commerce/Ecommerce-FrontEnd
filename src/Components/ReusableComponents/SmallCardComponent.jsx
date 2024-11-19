244f7996fb0cfa7e45dc77db26cdd1b9186d53a0
rohitmangal4 <mangaleswaran2001@gmail.com>
2024-11-19 15:37:44 +0530

    bike card created

--- src/Components/ReusableComponents/SmallCardComponent.jsx
+++ src/Components/ReusableComponents/SmallCardComponent.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import Brand from "../images/bajaj 1.png";
-import Bike from "../images/135 dti 1.png";
+
 
 const CardDesign = ({ value }) => {
   const { name, brand, bike } = value || {};
 
   return (
-    <div className="w-[300px] min-h-auto bg-white flex flex-col justify-center items-center gap-4 p-4 border-2 border-slate-800 shadow-md shadow-slate-500 rounded-[30px]">
-      <div className="w-full h-auto flex justify-between items-start font-bold text-2xl">
-        <p>{name}</p>
-        {brand}
+    <div className="w-[300px] min-h-auto bg-gradient-to-tl from-[#0E4257] to-[#058CA6]  flex flex-col justify-center items-center gap-4 p-4  shadow-lg shadow-slate-500 rounded-[30px]">
+      <div className="w-full h-auto flex justify-between items-center font-extrabold text-2xl " style={{textShadow: "0px 0px 5px #FFFFFF"}}>
+        <img src={brand} alt="bikeImage" className="w-16 h-10"/>
+        <p className="text-black">{name}</p>
       </div>
       <div>
-        {bike}
+        <img src={bike} alt="" className="w-full h-[200px] object-cover rounded-[30px]"/>
       </div>
     </div>
   );
@@ -22,18 +21,18 @@ const BikeCardMain = () => {
   const bikeData = [
     {
       name: "135 DTSI",
-      brand: <img src={Brand} alt="Brand" className="w-16 h-16"/>,
-      bike: <img src={Bike} alt="Bike" />,
+      brand: 'https://automarken-liste.com/wp-content/uploads/2021/02/Bajaj-Auto-Logo-1536x1152.png',
+      bike: 'https://moto.zombdrive.com/images1600_/bajaj-discover-135-dtsi-2010-1.jpg',
     },
     {
       name: "135 DTSI",
-      brand: <img src={Brand} alt="Brand" className="w-16 h-16" />,
-      bike: <img src={Bike} alt="Bike" />,
+      brand: 'https://automarken-liste.com/wp-content/uploads/2021/02/Bajaj-Auto-Logo-1536x1152.png',
+      bike: 'https://moto.zombdrive.com/images1600_/bajaj-discover-135-dtsi-2010-1.jpg',
     },
     {
       name: "135 DTSI",
-      brand: <img src={Brand} alt="Brand" className="w-16 h-16" />,
-      bike: <img src={Bike} alt="Bike" />,
+      brand: 'https://automarken-liste.com/wp-content/uploads/2021/02/Bajaj-Auto-Logo-1536x1152.png',
+      bike: 'https://moto.zombdrive.com/images1600_/bajaj-discover-135-dtsi-2010-1.jpg',
     },
   ];
 
